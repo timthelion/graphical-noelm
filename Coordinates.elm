ad0317bfe45962ff5c0986174b63945da054082d
Timothy Hobbs <timothyhobbs@seznam.cz>
2013-10-13 15:03:29 +0200

    Moved helper functions out to modules.

---
+++ Coordinates.elm
@@ -0,0 +1,34 @@
+module Coordinates where
+import open List
+import open Graph
+
+type Coord = {x:Int,y:Int}
+
+coordinates: Graph -> [(Coord,Node)]
+coordinates graph = levelizeGraph graph |> reverse |> coordinates' 0 |> concat
+
+coordinates': Int -> [[Node]] -> [[(Coord,Node)]]
+coordinates' n nodes' =
+ case nodes' of
+  (nodes::nodess) -> (coordinates'' nodes 0 n)::coordinates' (n+1) nodess
+  [] -> []
+coordinates'': [Node] -> Int -> Int -> [(Coord,Node)]
+coordinates'' nodes' x y =
+ case nodes' of
+  (node::nodes) -> ({x=x,y=y},node)::coordinates'' nodes (x+1) y
+  [] -> []
+
+getCoord: String -> [(Coord,Node)] -> Coord
+getCoord name coords =
+ case coords of
+  ((coord,node)::coords') ->
+   if | name==node.name -> coord
+      | otherwise -> getCoord name coords'
+
+getNode: Coord -> [(Coord,Node)] -> Node
+getNode coordToGet coords =
+ case coords of
+  ((coord,node)::coords') ->
+   if | coordToGet==coord -> node
+      | otherwise -> getNode coordToGet coords'
+  [] -> emptyNode
--- GraphicalElm.elm
+++ GraphicalElm.elm
@@ -1,64 +1,8 @@
 module GraphicalElm where
 import open List
 import Keyboard
-
-type Graph = [Node]
-data Language = ElmLang
-type Value = {code:String,language:Language}
-type Node = {parents:[String],name:String,value:Value}
-
-emptyNode = {parents=[],name="",value=defaultValue}
-defaultValue={code="",language=ElmLang}
-
-data Ord = Gt | Lt | Eq
-
-naiveSortBy: (a -> a -> Ord) -> [a] -> [a]
-naiveSortBy comp al = naiveSortBy' comp al [] []
-naiveSortBy' comp source bucketLess bucketMore =
- case source of
-  (a1::a2::ass) -> case comp a1 a2 of
-                   Gt -> naiveSortBy' comp (a2::ass) (bucketLess) (a1::bucketMore)
-                   Lt -> naiveSortBy' comp (a2::ass) (bucketLess++[a1]) bucketMore
-                   Eq -> naiveSortBy' comp (a2::ass) (bucketLess++[a1]) bucketMore
-  (a::[]) -> case (bucketLess,bucketMore) of
-              ([],[]) -> [a]
-              ((bl::bls),[]) -> case comp a (last bucketLess) of
-                                Gt -> bucketLess++[a]
-                                Eq -> bucketLess++[a]
-                                Lt -> naiveSortBy' comp (a::bucketLess) [] []
-              (_,_) -> naiveSortBy' comp (bucketLess++[a]++bucketMore) [] []
-  [] -> case bucketMore of
-         [] -> bucketLess
-         _ -> naiveSortBy' comp (bucketLess++bucketMore) [] []
-
---VERY NAIVE IMPLEMENTATION, very inefficient, goes into infinite loop in case of graph cyclicity.
-sortGraph: Graph -> Graph
-sortGraph g = reverse <| sortGraph' [] <| naiveSortBy (\n1 n2-> if | length n1.parents > length n2.parents -> Gt
-                                                                   | length n1.parents == length n2.parents -> Eq
-                                                                   | otherwise -> Lt) g
-
-sortGraph' alreadySorted ns =
- case ns of
-  (node::nodes) ->
-    let inAlreadySorted n = any (\asn->asn.name==n) alreadySorted
-    in
-    if | any inAlreadySorted node.parents || node.parents==[] -> sortGraph' (node::alreadySorted) nodes
-       | otherwise -> sortGraph' alreadySorted <| nodes ++(node::[])
-  [] -> alreadySorted
-
-levelizeGraph: Graph -> [[Node]]
-levelizeGraph g = levelizeGraph' [] <| sortGraph g
-levelizeGraph': [[Node]] -> Graph -> [[Node]]
-levelizeGraph' ls ns =
- case (ls,ns) of
-   ((thisLevel::higherLevels),(node::nodes))->
-    let
-     hasParentInThisLevel = any (\pp-> any (\p->p==pp.name) node.parents) thisLevel
-    in
-    if | hasParentInThisLevel -> levelizeGraph' ([node]::thisLevel::higherLevels) nodes
-       | otherwise -> levelizeGraph' ((node::thisLevel)::higherLevels) nodes
-   ([],(node::nodes)) ->  levelizeGraph' [[node]] nodes
-   (levels,[]) -> levels
+import open Graph
+import open Coordinates
 
 sampleGraph =
  [{parents=[],name="arrows",value=defaultValue}
@@ -70,37 +14,6 @@ sampleGraph =
 type GraphEditorState =
  {selectedNode: String
  ,graph: Graph}
-
-type Coord = {x:Int,y:Int}
-
-coordinates: Graph -> [(Coord,Node)]
-coordinates graph = levelizeGraph graph |> reverse |> coordinates' 0 |> concat
-
-coordinates': Int -> [[Node]] -> [[(Coord,Node)]]
-coordinates' n nodes' =
- case nodes' of
-  (nodes::nodess) -> (coordinates'' nodes 0 n)::coordinates' (n+1) nodess
-  [] -> []
-coordinates'': [Node] -> Int -> Int -> [(Coord,Node)]
-coordinates'' nodes' x y =
- case nodes' of
-  (node::nodes) -> ({x=x,y=y},node)::coordinates'' nodes (x+1) y
-  [] -> []
-
-getCoord: String -> [(Coord,Node)] -> Coord
-getCoord name coords =
- case coords of
-  ((coord,node)::coords') ->
-   if | name==node.name -> coord
-      | otherwise -> getCoord name coords'
-
-getNode: Coord -> [(Coord,Node)] -> Node
-getNode coordToGet coords =
- case coords of
-  ((coord,node)::coords') ->
-   if | coordToGet==coord -> node
-      | otherwise -> getNode coordToGet coords'
-  [] -> emptyNode
 
 graphEditorState =
  foldp
