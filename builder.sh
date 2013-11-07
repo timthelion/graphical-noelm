be546686c994e49b3de9e10c9e17967c36f8ee9b
Timothy Vladimír Hobbs <timothyhobbs@seznam.cz>
2013-11-07 13:03:40 +0100

    spiffed out build script
    - Allow the user to simultaneously save to a file.
    - Cat that file at the start, so the user can copy that output right into graphical elm.
    - Backup that file at the start, incase the user submits a blank or something.

--- builder.sh
+++ builder.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+if [ "$1" != ""  ];
+ then
+ if [ "$2" != "--no-display-old-contents" ]
+  then
+   cat $1
+   echo "Backing up file to .$1.bk"
+   cp  $1 .$1.bk
+  else echo "";
+ fi
+ else echo "No file to load...";
+fi
 echo "Paste your program here and press Control-D to build."
 cat > program.elm
+if [ "$1" != "" ];
+ then
+  echo "Saving code to $1..."
+  cp program.elm $1;
+ else
+  echo "No filename provided, won't save to separate file.";
+fi
 elm program.elm
-firefox build/program.html
-bash builder.sh
+chromium build/program.html
+bash builder.sh $1 --no-display-old-contents
